d382dbf6133faf022dfe527915eb7df7e899f3ee
Gregor Purdy <gregor@abcelo.com>
2021-03-19 19:42:26 -0700

    Add a benchmark for parsing, for now with just a single case

---
+++ benches/parse.rs
@@ -0,0 +1,16 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use isin;
+
+// const ISIN_STRINGS: [&str; 3] = [
+//     "AA0000000005", // The least taxing input for the functional style because digit expansion is rarely needed
+//     "US0378331005", // A typical input (this is the payload for the Apple (AAPL) commons stock ISIN)
+//     "ZZZZZZZZZZZ5", // The most taxing input for the functional style because digit expansion is maximized
+// ];
+
+fn bench_parses(c: &mut Criterion) {
+    c.bench_function("x",|b| b.iter(|| isin::parse_strict(black_box("US0378331005"))));
+}
+
+criterion_group!(benches, bench_parses);
+criterion_main!(benches);
