627dffe9cffc9fd27021105e0a91adaf70f2556d
Gregor Purdy <gregor@abcelo.com>
2022-05-29 11:04:39 -0700

    Add more parse test cases (same set as for checksum)

--- benches/parse.rs
+++ benches/parse.rs
@@ -2,14 +2,20 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use isin;
 
-// const ISIN_STRINGS: [&str; 3] = [
-//     "AA0000000005", // The least taxing input for the functional style because digit expansion is rarely needed
-//     "US0378331005", // A typical input (this is the payload for the Apple (AAPL) commons stock ISIN)
-//     "ZZZZZZZZZZZ5", // The most taxing input for the functional style because digit expansion is maximized
-// ];
+const ISIN_STRINGS: [&str; 3] = [
+    "AA0000000005", // The least taxing input for the functional style because digit expansion is rarely needed
+    "US0378331005", // A typical input (this is the payload for the Apple (AAPL) commons stock ISIN)
+    "ZZZZZZZZZZZ5", // The most taxing input for the functional style because digit expansion is maximized
+];
 
 fn bench_parses(c: &mut Criterion) {
-    c.bench_function("x", |b| b.iter(|| isin::parse(black_box("US0378331005"))));
+    let mut group = c.benchmark_group("Parse");
+
+    for p in ISIN_STRINGS.iter() {
+        group.bench_function(*p, |b| b.iter(|| isin::parse(black_box(p))));
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, bench_parses);
